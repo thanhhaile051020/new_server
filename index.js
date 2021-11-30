2dcae944650fbd4b192816785f0d4bf183f5c6e1
thanhhaile0510@gmail.com <thanhhaile0510@gmail.com>
2021-11-30 23:28:50 +0700

    cc

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { instrument } = require("@socket.io/admin-ui");
 const io = require("socket.io")(4001, {
   cors: {
-    origin: "*",
-  }, 
+    origin:"*",
+  },
 });
 const mongoose = require("mongoose");
 const authRouter = require("./routes/auth");
@@ -15,6 +15,7 @@ const uploadRouter = require("./routes/upload");
 const groupRouter = require("./routes/group");
 const helmet = require("helmet");
 const path = require("path");
+const { cloudinary } = require("./util/cloudinary");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -37,6 +38,8 @@ connectDB();
 
 const app = express();
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(
   cors({
     origin: "*",
@@ -45,5 +48,48 @@ app.use(
 app.use(helmet());
 
 
-const PORT = process.env.PORT || 5000;
+const userNotification = io.of("/notification");
+userNotification.on("connection", (socket) => {
+  // console.log("1", socket.id);
+});
+// userNotification.use((socket, next) => {
+//   if (socket.handshake.auth.token) {
+//     socket.username;
+//     next();
+//   } else {
+//     next(new Error("Error token"));
+//   }
+// });
+io.on("connection", (socket) => {
+  // console.log("1", socket.id);
+  // socket.on('test',(rq)=>{
+  //   console.log('sss',rq)
+  //  io.emit('receive-message','server rs')
+  // })
+  // socket.to(room).emit("receive-noti",'noti')
+  socket.on("join-room", (room) => {
+    var rooms = io.sockets.adapter.sids[socket.id];
+    for (var room in rooms) {
+      socket.leave(room);
+    }
+    console.log("join", room);
+    socket.join(room);
+  });
+});
+
+app.use(function (req, res, next) {
+  req.io = io;
+  next();
+});
+app.use("/api/auth", authRouter);
+app.use("/api/post", postRouter);
+app.use("/api/comment", commentRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/group", groupRouter);
+app.use("/api/notification", usersNotificationRouter);
+app.use("/api/upload", uploadRouter.routes);
+app.use("/filemanager", express.static(path.join(__dirname, "uploads")));
+
+instrument(io, { auth: false });
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
